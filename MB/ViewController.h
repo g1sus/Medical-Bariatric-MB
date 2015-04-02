86ac6bcb3aa6cab30b943f42db754aa0b0a75589
g1sus <proyectos_technology@hotmail.com>
2015-04-02 00:10:27 -0600

    add view

---
+++ MB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MB
+//
+//  Created by Jesus Antonio Hernandez Mendez on 31/03/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
